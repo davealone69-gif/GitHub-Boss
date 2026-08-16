8391029d0e21209ac686ce2728eaecb1e12ff931
davealone69-gif <davealone69@gmail.com>
2026-08-17 03:41:47 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,4 +72,5 @@ dependencies {
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)}
+    androidTestImplementation(libs.androidx.espresso.core)
+}
